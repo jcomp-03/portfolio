1c15f12d6b92e4d964e41c66aad9df6b649c320c
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-08-19 17:01:16 -0400

    Saving. Branching to enhancement/animation-javascript

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const tooltips = document.querySelectorAll('.tt');
-tooltips.forEach(tt => {
-    new bootstrap.Tooltip(tt);
-})
+console.log('Script filed is read');
+const jumbotron = document.getElementById("jumbotron");
+let animationCount = 0;
+const dashValues = [.05, .05, .02, .02, .04, .02, .06, .04, .15, .03];
+const pathNodeList = document.get
+function beginJourney(e) {
+
+    if(!animationCount) {
+
+    }
+    let target = e.target;
+    console.log("The callback function was invoked");
+}
+
+jumbotron.addEventListener('mouseenter', beginJourney);
+
+// const tooltips = document.querySelectorAll('.tt');
+// tooltips.forEach(tt => {
+//     new bootstrap.Tooltip(tt);
+// })
