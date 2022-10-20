d18f91dd1c4e859959d886c0f9e05eb0c64835bd
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-10-19 21:34:58 -0400

    Render build failed. Added path module to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const path = require('path');
 const PORT = process.env.PORT || 8000;
 require("dotenv").config();
 
